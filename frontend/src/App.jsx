671cbc87ea35147d14bbae6b15415856e9ca8d5a
Lakshmi Deepthi Chittajallu <chlakshmideepthi06@gmail.com>
2026-01-03 15:54:41 +0530

    Fixed all the errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
 import { AuthProvider, useAuth } from './context/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
@@ -20,7 +22,7 @@ function AppRouter() {
       <Route path="/login" element={<LoginPage />} />
 
       <Route path="/register" element={<RegisterPage />} />
-      
+
       <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
 
       <Route path="/users" element={<ProtectedRoute><UsersPage /></ProtectedRoute>} />
